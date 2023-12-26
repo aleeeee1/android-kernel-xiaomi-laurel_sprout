78612f2f2f2796c548594fd344457ff2b6a108d6
aleeeee1 <pace.alessandro05@protonmail.com>
2023-12-26 09:31:55 +0000

    slight changes to build.sh

--- build.sh
+++ build.sh
@@ -12,15 +12,16 @@ export USE_CCACHE=1
 export CCACHE_EXEC=$(command -v ccache)
 
 [ -d out ] && rm -rf out
-make vendor/laurel_sprout-perf_defconfig O=out
-make -j8 -k CC="ccache clang" O=out CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi-
-make -C AnyKernel3 clean
+make -j $(nproc --all) vendor/laurel_sprout-perf_defconfig O=out
+make -j $(nproc --all) -k CC="ccache clang" O=out CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+make -j $(nproc --all) -C AnyKernel3 clean
 
 if [ "$?" == 0 ]; then
    cp $(pwd)/out/arch/arm64/boot/Image.gz-dtb $(pwd)/AnyKernel3
    cp $(pwd)/out/arch/arm64/boot/dtbo.img $(pwd)/AnyKernel3
+   cp $(pwd)/out/arch/arm64/boot/dtb.img $(pwd)/AnyKernel3
    cd $(pwd)/AnyKernel3
-   make -j8
+   make -j $(nproc --all)
    zipname=$(ls *.zip)
    url=$(curl --upload-file ./${zipname} https://transfer.sh/${zipname})
    echo -e "\nKernel Url:"
